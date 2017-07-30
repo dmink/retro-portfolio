38cff4ba15b1770817e4966899e46eb3d5b0cd78
Dmitry Kostrubiak <dminked@gmail.com>
2017-07-30 18:56:35 +0300

    changed the folder and files structure and removed gulp

--- source/templates/sections/global/footer.php
+++
@@ -1,6 +0,0 @@
-		<script src="js/jquery-3.1.0.min.js"></script>
-		<script src="js/slick-1.6.0.min.js"></script>
-		<script src="js/jquery.mixitup.min.js"></script>
-		<script src="js/customization.js"></script>
-	</body>
-</html>
---
+++ templates/sections/global/header.php
@@ -0,0 +1,37 @@
+<!-- BEGIN .top-header -->
+<div id="anchor-top" class="top-header">
+
+    <!-- BEGIN .top-header__container -->
+    <div class="top-header__container">
+
+        <nav class="main-navigation">
+
+            <span class="menu-mobile js-menu-btn">
+                <a class="menu-mobile__link" href="#"><span class="menu-mobile__title">Menu</span><span class="menu-mobile__logo">dm</span></a>
+            </span>
+
+            <ul class="main-menu js-menu-mobile">
+                <li class="main-menu__item main-menu__item--about">
+                    <a class="main-menu__link" href="/">About me</a>
+                </li>
+                <li class="main-menu__item main-menu__item--portfolio">
+                    <a class="main-menu__link" href="portfolio.php">Portfolio</a>
+                </li>
+                <li class="main-menu__item main-menu__item--logo">
+                    <a class="main-menu__link main-menu__link--logo"><span class="main-menu__link--name">dm</span></a>
+                </li>
+                <li class="main-menu__item main-menu__item--blog">
+                    <a class="main-menu__link" href="blog.php">My blog</a>
+                </li>
+                <li class="main-menu__item main-menu__item--contact">
+                    <a class="main-menu__link" href="contact.php">Contact me</a>
+                </li>
+            </ul>
+
+        </nav>
+
+    </div>
+    <!-- END .top-header__container -->
+
+</div>
+<!-- END .top-header -->
